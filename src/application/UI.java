494660fc563d39799fb0a79268d26bab333d1d12
Pedro Quintanilha <pedrosquinta@gmail.com>
2022-07-15 13:07:11 -0300

    Pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -115,6 +115,7 @@ public class UI {
 	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println();
 		System.out.println("Captured pieces:");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
